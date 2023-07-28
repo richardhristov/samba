3130c9cee065ff069639486e093a9cd1889896b1
Richard Hristov <richardhristov@gmail.com>
2023-07-28 16:37:32 +0300

    remove unnecessary chcon

--- setup.sh
+++ setup.sh
@@ -5,6 +5,3 @@ mkdir -p data/timemachinebackup
 
 chown -R 3000:3000 data/share
 chown -R 3000:3000 data/timemachinebackup
-
-chcon -R -t svirt_sandbox_file_t data/share
-chcon -R -t svirt_sandbox_file_t data/timemachinebackup
