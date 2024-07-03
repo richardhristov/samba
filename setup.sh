9baf8d0adaa64846c9c11388dc4a85d6e5bdd1ad
Richard Hristov <richardhristov@gmail.com>
2024-07-03 20:26:12 +0300

    update config

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 mkdir -p data/share
-mkdir -p data/share/torrent-config
-mkdir -p data/share/Downloads
 mkdir -p data/timemachinebackup
+mkdir -p data/share/Downloads
+mkdir -p data/share/Syncthing
+mkdir -p data/torrent-config
+mkdir -p data/syncthing-config
 
-chown -R 3000:3000 data/share
-chown -R 3000:3000 data/timemachinebackup
+chown -R 3000:3000 data
