fdfdc6a9fca7b5f36b6c40f07b5f263c3019c247
Richard Hristov <richardhristov@gmail.com>
2025-03-17 14:35:26 +0200

    remove syncthing

--- setup.sh
+++ setup.sh
@@ -3,8 +3,6 @@
 mkdir -p data/share
 mkdir -p data/timemachinebackup
 mkdir -p data/share/Downloads
-mkdir -p data/share/Syncthing
 mkdir -p data/torrent-config
-mkdir -p data/syncthing-config
 
 chown -R 3000:3000 data
